779f9ce96ab1eaa4c7ffca7d169a0688baadcbd7
mothmelons <alexkulll001@gmail.com>
2025-04-07 16:52:33 +0300

    Колір букв змінюється разом з темою

--- Spotify/script.js
+++ Spotify/script.js
@@ -6,6 +6,8 @@ function showusername(){
 
 const DARK_MODE = 'black';
 const LIGHT_MODE = 'white';
+const DARK_MODE_TEXT = 'white'
+const LIGHT_MODE_TEXT = 'black'
 let current_background_color = DARK_MODE;
 
 function change_theme() {
@@ -13,10 +15,14 @@ function change_theme() {
     if (body) {
         if (current_background_color === DARK_MODE) {
             current_background_color = LIGHT_MODE;
+            current_color = LIGHT_MODE_TEXT;
         }else {
             current_background_color = DARK_MODE;
+            current_color = DARK_MODE_TEXT;
+            
             }
         body.style.backgroundColor = current_background_color;
+        body.style.color = current_color;
         body.style.backgroundSize = "cover";
         }
     }
